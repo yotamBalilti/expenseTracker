a254ec7e12305ef907b31dd61beee18af5653a30
Yotam <yotam.balilti@gmail.com>
2020-12-26 19:31:17 +0200

    added List component

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -14,7 +14,68 @@ import { Delete, MoneyOff } from "@material-ui/icons";
 import useStyles from "./styles";
 
 const List = () => {
-  return <div></div>;
+  const classes = useStyles();
+
+  const transactions = [
+    {
+      id: 1,
+      type: "Income",
+      category: "Salary",
+      amount: 50,
+      date: "Sat Dec 26",
+    },
+    {
+      id: 2,
+      type: "Expense",
+      category: "Pets",
+      amount: 150,
+      date: "Sat Dec 22",
+    },
+    {
+      id: 3,
+      type: "Income",
+      category: "Business",
+      amount: 250,
+      date: "Sat Dec 24",
+    },
+  ];
+
+  return (
+    <MUIList dense={false} className={classes.list}>
+      {transactions.map(transaction => (
+        <Slide
+          direction="down"
+          in
+          mountOnEnter
+          unmountOnExit
+          key={transaction.id}
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar
+                className={
+                  transaction.type === "Income"
+                    ? classes.avatarIncome
+                    : classes.avatarExpense
+                }
+              >
+                <MoneyOff />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={transaction.category}
+              secondary={`$${transaction.amount} - ${transaction.date}`}
+            />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" onClick="">
+                <Delete />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Slide>
+      ))}
+    </MUIList>
+  );
 };
 
 export default List;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,6 +10,7 @@ import {
 
 import useStyles from "./styles";
 import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
   const classes = useStyles();
@@ -32,7 +33,7 @@ const Main = () => {
         <CardContent className={classes.cartContent}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              {/* List */}
+              <List />
             </Grid>
           </Grid>
         </CardContent>
